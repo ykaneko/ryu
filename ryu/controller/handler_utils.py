2a35f4b659e7ac0c1dc30d729cdf6650c50c4f27
Isaku Yamahata <yamahata@valinux.co.jp>
2012-11-30 14:09:44 +0900

    ryu/controller/handler_utils.py: simplify RequestQueue by task_queue
    code simplification by task_queue.
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- ryu/controller/handler_utils.py
+++ ryu/controller/handler_utils.py
@@ -20,6 +20,7 @@ import types
 from ryu.controller import (dispatcher,
                             event,
                             handler)
+from ryu.lib import task_queue
 
 
 LOG = logging.getLogger(__name__)
@@ -60,58 +61,29 @@ class QueueSerializer(object):
 
 
 class RequestQueue(object):
-    class _EventRequestBase(event.EventBase):
-        def __init__(self, *args):
-            super(RequestQueue._EventRequestBase, self).__init__()
-            self.args = args
+    @classmethod
+    def _create_request_method(cls, method):
+        # keyword, default argument aren't supported
+        request_func_name = 'request_%s' % method.__name__.lstrip('_')
+        request_func = lambda self, *args: self._task_q.queue(
+            lambda: method(self, *args))
+        request_method = types.MethodType(request_func, None, cls)
+        _cls_set_attr(cls, request_func_name, request_method)
 
     @classmethod
-    def _create_request_methods(cls, dispatcher_name, methods):
-        # variable, keyword, default argument aren't supported
-        ev_dispatcher = dispatcher.EventDispatcher(dispatcher_name)
-        cls._cls_ev_dispatcher = ev_dispatcher
-
+    def _create_request_methods(cls, methods):
         for method in methods:
-            method_name_no_strip = method.__name__
-            method_name = method_name_no_strip.lstrip('_')
-
-            # create event class _EventRequest_<method_name>
-            ev_cls_name = '_EventRequest_%s' % method_name
-            ev_cls = type(ev_cls_name, (cls._EventRequestBase,),
-                          dict(__init__=lambda self, *args:
-                               super(self.__class__, self).__init__(*args)))
-            _cls_set_attr(cls, ev_cls_name, ev_cls)
-
-            # create request_<method_name>(self, <args>) method
-            request_func_name = 'request_%s' % method_name
-            request_func = eval(
-                'lambda self, *args: self._ev_q.queue(self.%s(*args))' %
-                ev_cls_name)
-            request_method = types.MethodType(request_func, None, cls)
-            _cls_set_attr(cls, request_func_name, request_method)
-
-            # create <method_name>_handler(self, <args>) method
-            handler_name = '%s_handler' % method_name
-            handler_ = eval('lambda self, ev: self.%s(*ev.args)' %
-                            method_name_no_strip)
-            handler_ = handler.set_ev_cls(ev_cls, ev_dispatcher)(handler_)
-            method_ = types.MethodType(handler_, None, cls)
-            _cls_set_attr(cls, handler_name, method_)
+            # NOTE: don't inline _create_request_method() because it uses
+            #       closure that references the variable, method.
+            cls._create_request_method(method)
 
     def __init__(self, methods):
         super(RequestQueue, self).__init__()
-
-        cls = self.__class__
-        postfix = '%s_%x' % (cls.__name__, id(cls))
-        queue_name = 'queue_name_%s' % postfix
-        dispatcher_name = 'dispatcher_name_%s' % postfix
-        cls_init_once(self._create_request_methods, dispatcher_name, methods)
-        self._ev_q = dispatcher.EventQueueThread(queue_name,
-                                                 self._cls_ev_dispatcher)
-        handler.register_instance(self)
+        cls_init_once(self._create_request_methods, methods)
+        self._task_q = task_queue.TaskQueue()
 
     def close(self):
-        self._ev_q.close()
+        self._task_q.close()
 
 
 def test():
@@ -139,14 +111,6 @@ def test():
     rqt.method1('a')
     rqt.method2('b', 'c')
     rqt.no_method()
-
-    ev0 = RequestQueueTest._EventRequest_method0()
-    rqt.method0_handler(ev0)
-    ev1 = RequestQueueTest._EventRequest_method1('A')
-    rqt.method1_handler(ev1)
-    ev2 = RequestQueueTest._EventRequest_method0('B', 'C')
-    rqt.method2_handler(ev2)
-    assert not hasattr(rqt, 'no_method_handler')
 
     rqt.request_method0()
     rqt.request_method1(1)
